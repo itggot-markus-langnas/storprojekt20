7fc4828f3a29f07c12b2e250a3d3f57e9e3fd87c
itggot-markus-langnas <43438030+itggot-markus-langnas@users.noreply.github.com>
2020-04-30 10:46:44 +0200

    .

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require 'bcrypt'
 enable :sessions
 
 get('/')do
-db = SQLite3::Database.new("db/chatt.db")
-slim(:index)
+  db = SQLite3::Database.new("db/chatt.db")
+  slim(:index)
 end 
 
 def set_error(error_message)
@@ -19,14 +19,15 @@ def connect_to_db(path)
     db = SQLite3::Database.new("db/chatt.db")
     db.results_as_hash = true
     return db
-   end
+end
    
 
 get('/error')do
-slim(:error)
+  slim(:error)
 end
 
 post('/register') do
+    session.clear
     db = SQLite3::Database.new("db/chatt.db")
     db.results_as_hash = true
     username= params["username"]
@@ -49,34 +50,63 @@ post('/register') do
         end
     else
         p 1
-        set_error("username nalready exist")
+        
+set_error("username already exist")
         redirect('/error')
     end
-            
 end
 
 post('/login') do
-    db = SQLite3::Database.new("db/chatt.db")
-    db.results_as_hash = true
-    username= params["username"]
-    password= params["password"]
-    result = db.execute("SELECT id, password_digest FROM users WHERE username=?", [username])
+  db = SQLite3::Database.new("db/chatt.db")
+  db.results_as_hash = true
+  username= params["username"]
+  password= params["password"]
+  result = db.execute("SELECT id, password_digest, admin FROM users WHERE username=?", [username])
+  if result.empty?
+    set_error("Något gick fel, försök igen")
+    redirect('/error')
+  end
 
-    if result.empty?
-        set_error("Du behöver skriva in något för att kunna logga in")
-        redirect('/error')
-    end
+  if session[:attempt].nil?
+    session[:attempt] = Time.now
+elsif Time.now - session[:attempt] < 20 
+    session[:error] = "Du har slagit in fel lösenord, försök igen om en stund"
+    redirect('/error')
+end 
 
-    user_id = result.first["id"]
-    password_digest = result.first["password_digest"]
-    if BCrypt::Password.new(password_digest)==password
-        session[:user_id] = user_id
-        redirect('/reg_confirm')
-    else
-        set_error("Uppgifterna du skrev in var tyvärr fel")
-        redirect('/error')
-    end
+session[:attempt] = Time.now
+
+
+  user_id = result.first["id"]
+  password_digest = result.first["password_digest"]
+  if BCrypt::Password.new(password_digest)==password
+      session[:user_id] = user_id
+      if result.first["admin"].to_s == '1'
+        admin = true
+      else
+        admin = false
+      end
+      session[:admin] = admin
+  else
+    # Wrong password
+      redirect '/'
+  end
+  # Correct password and successfully signed in
+  redirect '/reg_confirm'
 end
+
+get '/admin/*' do
+    if session[:admin] == true
+      # It´s an admin
+    else
+      # Not a admin
+        redirect '/'
+    end
+
+    slim :admin
+end
+
+
 
 get('/reg_confirm') do
     user_id = session[:user_id]
@@ -84,24 +114,24 @@ get('/reg_confirm') do
     db.results_as_hash = true
     search = session[:search]
     result = db.execute("SELECT * FROM users WHERE username LIKE ?", '%'+search.to_s+'%')
+    admin = session[:admin]
     p result
-    slim(:"lists/index1",locals:{chatt:result})
+    slim(:"lists/index1",locals:{chatt:result, admin: admin})
 end
 
 post('/search_user') do
     db = SQLite3::Database.new("db/chatt.db")
     db.results_as_hash = true
-
     search = params['username']
     p search
     session[:search] = search
-    
     redirect('/reg_confirm')
 end
 
 get('/:id/chat') do
     db = SQLite3::Database.new("db/chatt.db")
     db.results_as_hash = true
+    @admin=session['admin']
     reciever_id = params['id']
     user_id = session[:user_id]
     result = db.execute("SELECT * FROM messages WHERE reciever_id = ? AND sender_id = ? OR reciever_id = ? AND sender_id = ?", [reciever_id, user_id,  user_id, reciever_id])
@@ -120,4 +150,22 @@ post('/:id/chat') do
     timestamp = Time.now.to_s
     db.execute("INSERT INTO messages(sender_id, reciever_id, content, timestamp) VALUES(?,?,?,?)", [sender_id, reciever_id, message, timestamp])
     redirect("/#{reciever_id}/chat")
+end
+
+post('/:id/delete') do
+  db = SQLite3::Database.new("db/chatt.db")
+  db.results_as_hash = true
+  id = params['id']
+  db.execute('DELETE FROM users WHERE id=?', [id])
+  redirect '/reg_confirm'
+end
+
+get('/group') do
+  db = SQLite3::Database.new("db/chatt.db")
+  db.results_as_hash = true
+  user_id = session[:user_id]
+  search = session[:search]
+  result = db.execute("SELECT * FROM users WHERE username LIKE ?", '%'+search.to_s+'%')
+  p result
+  slim(:group)
 end
